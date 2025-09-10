7c65f25c1ca0a57cae0090fbbe709cea8c62131a
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2025-09-10 11:48:53 +0200

    added slow client

--- multiparadigm-concurrency/main.cpp
+++ multiparadigm-concurrency/main.cpp
@@ -3,6 +3,7 @@
 #include "ImageTracer.h"
 #include "ImageViewer.h"
 #include "RemoteControl.h"
+#include "SlowClient.h"
 
 static void SendKeyboardInputTo(const so_5::mbox_t& destination)
 {
@@ -23,6 +24,7 @@ int main()
 	const auto imageCh = environment.create_mbox("raw-images");
 	const auto commandsCh = environment.create_mbox("commands");
 	const auto keyboardCh = environment.create_mbox();
+	const auto slowClientCh = create_mchain(environment, 1, so_5::mchain_props::memory_usage_t::preallocated, so_5::mchain_props::overflow_reaction_t::drop_newest);
 
 	environment.introduce_coop([&](so_5::coop_t& coop) {
 		coop.make_agent<ImageProducer>(imageCh, commandsCh);
@@ -30,5 +32,8 @@ int main()
 		coop.make_agent<RemoteControl>(keyboardCh, commandsCh);
 	});
 
+	const auto slowClient = SpawnSlowClient(slowClientCh);
+
 	SendKeyboardInputTo(keyboardCh);
+	so_5::close_drop_content(so_5::exceptions_enabled, slowClientCh);
 }
